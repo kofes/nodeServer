5bdee5fba35fea2333c4635a94f9441447252d8a
kofes <turnever@gmail.com>
2017-07-17 18:31:46 +1000

    creation of session init

--- backend/main.js
+++ backend/main.js
@@ -10,7 +10,9 @@ const util = require('util');
 const formidable = require('formidable');
 const pg = require('pg');
 const crypto = require('crypto');
+
 const port = 8080;
+const expireTime = 3600000;
 
 const config = require('./config.js');
 const mailer = require('./mailer.js');
@@ -46,31 +48,59 @@ server.post('/submit', (request, response) => {
         }
     });
 });
-
+//Reset password
 server.get('/password/reset/:hash', (request, response) => {
     let hash = request.params.hash;
     pool.connect()
     .then(client => {
-        client.query('SELECT * FROM users WHERE hash = $1', [hash])
+        client.query('SELECT * FROM users WHERE hash = $1 AND hash_expire >= $2', [hash, Date.now()])
         .then(res => {
             if (typeof res === 'undefined' ||
-                typeof res.rows === 'undefined' ||
                 typeof res.rows[0] === 'undefined') {
                     util.log('bad hash from IP:' + request.address+';');
                     return err400('bad hash', response);
             }
-            util.log('good hash "'+res.rows[0].hash+'" from IP: '+request.address+';');
+            util.log('good hash for password reset from IP: '+request.address+';');
             //TODO: Reseting password
         })
     })
     .catch(err => {
-        err500(err, res);
+        err500(err, response);
     });
 });
 server.post('/password/reset/:hash', (request, response) => {
-
+    //Only json forms
+    if (request.get('content-type') !== 'application/json')
+        return err400('Wrong content-type: application/json only accepted', response);
+    let queryData = '';
+    let form = new formidable.IncomingForm();
+    form.parse(request, (err, fields, files) => {
+        if (err)
+            return response.status(413).send(err);
+        fields.hash = hash;
+        processResetPassword(request, response, fields);
+    });
 });
-
+//Confirm registration
+server.get('/register/:hash', (request, response) => {
+    let hash = request.params.hash;
+    pool.connect()
+    .then(client => {
+        client.query('SELECT * FROM users WHERE hash = $1 AND hash_expire >= $2', [hash, Date.now()])
+        .then(res => {
+            if (typeof res === 'undefined' || typeof res.rows[0] === 'undefined') {
+                    util.log('bad hash from IP:' + request.address+';');
+                    return err400('bad hash', response);
+            }
+            util.log('good hash for registration from IP: '+request.address+';');
+            //TODO: register now
+            client.query('UPDATE users SET hash_expire = NULL WHERE email = $1', [res.rows[0].email]);
+        });
+    })
+    .catch(err => {
+        err500(err, response);
+    })
+});
 pool.query(
     'CREATE TABLE IF NOT EXISTS users('+
     'id INTEGER PRIMARY KEY,'+
@@ -80,7 +110,8 @@ pool.query(
     'level INTEGER DEFAULT 1,'+
     'xp INTEGER DEFAULT 0,'+
     'coin INTEGER DEFAULT 0,'+
-    'hash VARCHAR(64) DEFAULT NULL'+
+    'hash VARCHAR(64) DEFAULT NULL,'+
+    'hash_expire NUMERIC DEFAULT NULL'+
 ')')
 .then(() => {
     server.listen(port, ()=> {
@@ -106,27 +137,29 @@ function processSendEmail(request, response, fields) {
                 util.log('bad email: '+ fields.email + ';');
                 return err400('email not exists', response);
             }
-            let userHash = crypto.randomBytes(48, (err,buff) => {
+            let userHash = crypto.randomBytes(20, (err,buff) => {
                 if (err)
                     util.log('user\'s hash encryption error: ' + err + ';');
-                return buff.toString('base64').replace(/\//g, '_').replace(/\+/g, '-');
+                return buff.toString('hex');
             });
-            if (typeof userHash == 'undefined')
+            if (typeof userHash === 'undefined')
                 return err500(undefined, response);
-            client.query('UPDATE users SET hash = $1 WHERE email = $2', [userHash, fields.email])
+            client.query('UPDATE users SET hash = $1, hash_expire = $2 WHERE email = $3',
+                        [userHash, Date.now()+expireTime, fields.email])
             .then(res => {
-                let address = request.protocol + '://' + request.get('host') + '/password/reset/' + userHash;
-                let text = 'If you want to reset your password,'+
-                        ' then click on the following link,'+
-                        ' or by copying and pasting it into your browser: '+
-                        address;
+                let address = request.protocol + '://' + request.headers.host + '/password/reset/' + userHash;
+                let text =
+                    'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+                    'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                    address + '\n\n' +
+                    'If you did not request this, please ignore this email and your password will remain unchanged.\n';
                 mailer.sendMail(
                     res.rows[0].email,     //user's email
                     'Reset your password', // title
                     text,
                     (err, info) => {
                     if (err) {
-                        client.query('UPDATE users SET hash = NULL WHERE email = $2', [userHash, fields.email]);
+                        client.query('UPDATE users SET hash_expire = NULL WHERE email = $1', [fields.email]);
                         return err500(err, response);
                     }
                     fs.readFile(verifyInfoPage, 'binary', (err, file) => {
@@ -168,12 +201,36 @@ function processSignUp(request, response, fields) {
                 if (err) util.log('password\'s encryption error: ' + err + ';');
                 return hash;
             });
-            if (typeof passwdHash === 'undefined')
+            let userHash = crypto.randomBytes(20, (err,buff) => {
+                if (err)
+                    util.log('user\'s hash encryption error: ' + err + ';');
+                return buff.toString('hex');
+            });
+            if (typeof passwdHash === 'undefined' || typeof userHash === 'undefined')
                 return err500(undefined, response);
-            client.query('INSERT INTO users(email, nickname, passwd) VALUES ($1, $2, $3)',
-                        [fields.email, fields.nickname, passwdHash])
+            client.query('INSERT INTO users(email, nickname, passwd, hash, hash_expire) VALUES ($1, $2, $3, $4, $5)',
+                        [fields.email, fields.nickname, passwdHash, hash, Date.now()+expireTime])
             .then(res => {
-                //TODO: make session & cookie
+                let address = request.protocol + '://' + request.headers.host + '/register/' + userHash;
+                let text =
+                    'You are receiving this because you (or someone else) have register on ' +
+                    serviceName + '.\n\n'+
+                    'Please click on the following link, or paste this into your browser to complete the registration:\n\n' +
+                    address + '\n\n' +
+                    'If you did not request this, please ignore this email and information about you will be deleted.\n';
+                mailer.sendMail(
+                    res.rows[0].email,     //user's email
+                    'Confirm your resgistration', // title
+                    text,
+                    (err, info) => {
+                    if (err) {
+                        client.query('DELETE FROM users WHERE email = $1', [fields.email]);
+                        return err500(err, response);
+                    }
+                    fs.readFile(verifyInfoPage, 'binary', (err, file) => {
+                        return response.status(200).send(err ? 'Verification email was sent to your email' : file);
+                    });
+                });
             });
         });
     })
@@ -208,8 +265,34 @@ function processSignIn(request, response, fields) {
                     util.log('wrong password from email "' + fields.email + '";');
                     return err400('wrong email/password', response);
                 }
-                client.query('UPDATE users SET hash = NULL WHERE email = $1', [fields.email]);
-                //TODO: accept user
+                client.query('UPDATE users SET hash_expire = NULL WHERE email = $1', [fields.email]);
+                //TODO: accept user -> enter to session
+            });
+        });
+    })
+    .catch(err => {
+        err500(err, response);
+    })
+}
+function processResetPassword(request, response, fields) {
+    if (!('passwd' in fields))
+        return err400(undefined, response);
+    if (!isValidPasswd(fields.passwd)) {
+        util.log('Not valid password of reseting;');
+        return err400('password not valid', response);
+    }
+    pool.connect()
+    .then(client => {
+        client.query('SELECT * FROM users WHERE hash = $1', [fields.hash])
+        .then(res => {
+            if (typeof res === 'undefined' || typeof res.rows[0] === 'undefined') {
+                    util.log('bad hash from IP:' + request.address+';');
+                    return err400('bad hash', response);
+            }
+            util.log('good hash for password reset from IP: '+request.address+';');
+            client.query('UPDATE users SET passwd = $1, hash_expire = NULL WHERE hash = $2', [fields.passwd, fields.hash])
+            .then(res => {
+                //TODO: accept user -> enter to session
             });
         });
     })
